bea91849143e398c4e79a8ea2889c16a0c758519
joachimcupani <joachim@cupani.fr>
2020-02-19 10:19:37 +0100

    correction bug toastify et loader react

--- assets/js/config.js
+++ assets/js/config.js
@@ -7,7 +7,7 @@
  *
  */
 
-export const API_URL = 'http://127.0.0.1:8000/api/';
+export const API_URL = process.env.API_URL;
 export const CUSTOMERS_API = API_URL + 'customers';
 export const INVOICES_API = API_URL + 'invoices';
 export const USERS_API = API_URL + 'users';
