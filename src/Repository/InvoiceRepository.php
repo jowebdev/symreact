2226e4d8d2eec2b5d67d2935b461e1c46eecd26a
joachimcupani <joachim@cupani.fr>
2020-02-19 11:04:44 +0100

    fix: Erreur sur le chrono lors de la première facture

--- src/Repository/InvoiceRepository.php
+++ src/Repository/InvoiceRepository.php
@@ -24,17 +24,20 @@ class InvoiceRepository extends ServiceEntityRepository
 
     public function findNextChrono(UserInterface $user)
     {
+        try {
+            return $this->createQueryBuilder('i')
+                    ->select('i.chrono')
+                    ->join('i.customer', 'c')
+                    ->andWhere('c.user = :user')
+                    ->setParameter('user', $user)
+                    ->orderBy('i.chrono', 'DESC')
+                    ->setMaxResults(1)
+                    ->getQuery()
+                    ->getSingleScalarResult() + 1;
+        } catch (\Exception $e) {
+            return 1;
+        }
 
-        return $this->createQueryBuilder('i')
-            ->select('i.chrono')
-            ->join('i.customer', 'c')
-            ->andWhere('c.user = :user')
-            ->setParameter('user', $user)
-            ->orderBy('i.chrono', 'DESC')
-            ->setMaxResults(1)
-            ->getQuery()
-            ->getSingleScalarResult() + 1
-            ;
     }
 
     // /**
